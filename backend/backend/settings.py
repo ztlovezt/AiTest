96d7cfd1ae78bf20a191a17fa20157aff19d4504
cnb <1275479178@qq.com>
2026-04-27 19:34:41 +0800

    增加daphne应用注册，解决daphne启动Websocket下无法远程连接手机问题

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -36,6 +36,7 @@ else:
                            cast=lambda v: [s.strip() for s in v.split(',')])
 
 DJANGO_APPS = [
+    'daphne',
     'simpleui',
     'django.contrib.admin',
     'django.contrib.auth',
